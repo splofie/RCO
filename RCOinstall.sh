eb3409efae2af1047ab811b0592c21cd794d3f6b
splofie <113855173+splofie@users.noreply.github.com>
2023-07-07 09:38:32 -0600

    Update RCOinstall.sh

--- RCOinstall.sh
+++ RCOinstall.sh
@@ -1,6 +1,6 @@
 cd /Applications/Roblox.app/Contents/MacOS
 echo "#!/bin/sh" >> RobloxPlayer
-echo "rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
+echo "rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Applications/Roblox.app/Contents/MacOS/RobloxPlayer" >> RobloxPlayer
 chmod +x RobloxPlayer
 touch RCOPATCHED
 echo "Finished installing RCO"
